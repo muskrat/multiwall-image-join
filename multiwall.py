db48ded2db4420b526f95dc86c2121f12e68048d
I K Stead <iste043@aucklanduni.ac.nz>
2012-09-25 17:25:07 +1200

    Updated comments and docstrings

--- multiwall.py
+++ multiwall.py
@@ -9,16 +9,23 @@ import Image    # Python imaging library
 import argparse # Command line options
 
 def open_images(path_list):
+    """Open images at paths and return list of Image.image objects"""
     return [Image.open(path) for path in path_list]
 
 def join_images(img_list):
+    """Join images horizontally into a new image"""
     sizes = [img.size for img in img_list] # (x, y) tuples
     
+    # Functions, just because I felt like doing this in functional style
     add = lambda a, b: a + b 
     elems = lambda i, l: [ x[i] for x in l ] # Get i'th element in each tuple
+
+    # Calculate size of new image by adding up x values and taking the
+    # maximum y value.
     newsize = (reduce(add, elems(0, sizes)), 
                     max(elems(1, sizes)))
 
+    # Create the new image and insert images from batch
     new = Image.new('RGB', newsize)
     topleft = 0
     for img in img_list:
@@ -27,6 +34,7 @@ def join_images(img_list):
     return new
 
 def save_image(img, path):
+    """Save image object to specified path"""
     img.save(path)
     return
 
